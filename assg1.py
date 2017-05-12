4313e8e2a8d0729a108667d85bfff2f5e4e098ff
sanathbk <sanathkshettar@gmail.com>
2017-05-12 12:08:05 +0530

    Update assg1.py

--- assg1.py
+++ assg1.py
@@ -2,7 +2,6 @@
 data=[1, 6, 4, 2, 5, 7, 6, 9, 5, 3, 4, 5, 9, 10, 2, 4, 5, 3, 6];
 n=len(data)
 p=int(raw_input("enter the width of class interval: "));
-l=min(data)
 a=max(data)
 n=int(a/p)
 for j in range(0,n):
